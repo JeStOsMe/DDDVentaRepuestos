6012bf8fd96c23da12a6d07ad043637f7788535b
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-13 17:24:33 -0500

    Estructura de objetos de valor terminada: módulos values creados dentro de cliente, vendedor y venta

---
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/cliente/values/CelularCliente.java
@@ -0,0 +1,5 @@
+package com.example.ventarepuestos.cliente.values;
+
+public class CelularCliente {
+    
+}
---
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/cliente/values/IdentificacionCliente.java
@@ -0,0 +1,5 @@
+package com.example.ventarepuestos.cliente.values;
+
+public class IdentificacionCliente {
+    
+}
