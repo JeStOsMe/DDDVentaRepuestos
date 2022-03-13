5dfdbcf6c1aff4e63870836fe820803b82d6bd3b
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-13 16:47:59 -0500

    Inicio del proyecto. Estado: recién iniciado con Spring Initialzr

---
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/VentarepuestosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ventarepuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentarepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentarepuestosApplication.class, args);
+	}
+
+}
---
+++ ventarepuestos/src/test/java/com/example/ventarepuestos/VentarepuestosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ventarepuestos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentarepuestosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
