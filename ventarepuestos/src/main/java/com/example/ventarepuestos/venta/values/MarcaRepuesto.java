858589ed4d6915c48353b2d3340ca46dd0903ca5
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 18:37:50 -0500

    PE. Objeto de valor MarcaRepuesto terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/MarcaRepuesto.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/MarcaRepuesto.java
@@ -1,5 +1,27 @@
 package com.example.ventarepuestos.venta.values;
 
-public class MarcaRepuesto {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class MarcaRepuesto implements ValueObject<String>{
+
+    private final String value;
+
+    public MarcaRepuesto(String value){
+        this.value = Objects.requireNonNull(value);
+
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El campo de marca no puede estar vacío");
+        }
+
+        if(this.value.length() < 5){
+            throw new IllegalArgumentException("La marca debe tener al menos 5 caracteres");
+        }
+    }
+
+    public String value(){
+        return value;
+    }
     
 }
