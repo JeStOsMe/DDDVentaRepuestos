b72ed8c301c87d0be6a9b9c341785c6885f87454
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 18:05:20 -0500

    PE. Objeto de valor Dinero terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/Dinero.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/Dinero.java
@@ -1,5 +1,27 @@
 package com.example.ventarepuestos.venta.values;
 
-public class Dinero {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Dinero implements ValueObject<String>{
+
+    private final String value;
+
+    public Dinero(String value){
+        this.value = Objects.requireNonNull(value);
+
+        if (this.value.isBlank()){
+            throw new IllegalArgumentException("El valor del dinero no puede estar vacío");
+        }
+
+        if (Double.parseDouble(this.value) < 0){
+            throw new IllegalArgumentException("El valor no puede ser negativo");
+        }
+    }
+
+    public String value(){
+        return value;
+    }
     
 }
