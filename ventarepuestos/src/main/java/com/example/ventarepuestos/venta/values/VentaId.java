d685251ff276960152cfd61bfee2bb5927f101f3
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 17:42:06 -0500

    PE. Objeto de valor VentaId terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/VentaId.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/VentaId.java
@@ -4,4 +4,15 @@ import co.com.sofka.domain.generic.Identity;
 
 public class VentaId extends Identity{
     
+    public VentaId(){
+
+    }
+
+    public VentaId(String id){
+        super(id);
+    }
+
+    public static VentaId of(String id){
+        return new VentaId(id);
+    }
 }
