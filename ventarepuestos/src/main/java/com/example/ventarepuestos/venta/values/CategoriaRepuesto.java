b61ec5f7cc31677371b87c906b2ae449799de8f5
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 17:54:04 -0500

    PE. Objeto de valor CategoriaRepuesto terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/CategoriaRepuesto.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/CategoriaRepuesto.java
@@ -1,5 +1,26 @@
 package com.example.ventarepuestos.venta.values;
 
-public class CategoriaRepuesto {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class CategoriaRepuesto implements ValueObject<String>{
+
+    private final String value;
+
+    public CategoriaRepuesto(String value){
+        this.value = Objects.requireNonNull(value);
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("La categoría no puede estar vacía");
+        }
+
+        if(this.value.length() <= 5){
+            throw new IllegalArgumentException("La categoría debe contener mínimo 5 caracteres");
+        }
+    }
+    
+    public String value(){
+        return value;
+    }
     
 }
