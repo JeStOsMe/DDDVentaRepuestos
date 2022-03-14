82220239e0a4a89288470e75930723d1a5dd348d
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 18:01:58 -0500

    PE. Objeto de valor CelularCajero terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/CelularCajero.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/CelularCajero.java
@@ -1,5 +1,27 @@
 package com.example.ventarepuestos.venta.values;
 
-public class CelularCajero {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class CelularCajero implements ValueObject<String>{
+
+    private final String value;
+
+    public CelularCajero(String value){
+        this.value = Objects.requireNonNull(value);
+
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El campo de celular no puede estar vacío");
+        }
+
+        if(this.value.length() < 10){
+            throw new IllegalArgumentException("La cantidad de dígitos debe ser mínimo de 10");
+        }
+    }
+
+    public String value(){
+        return value;
+    }
     
 }
