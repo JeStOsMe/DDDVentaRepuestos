0b5bfdefe25339e4e552eb3962f23741ddaaba33
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 18:41:24 -0500

    PE. Objeto de valor NombreCajero terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/NombreCajero.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/NombreCajero.java
@@ -1,5 +1,27 @@
 package com.example.ventarepuestos.venta.values;
 
-public class NombreCajero {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class NombreCajero implements ValueObject<String>{
+
+    private final String value;
+
+    public NombreCajero(String value){
+        this.value = Objects.requireNonNull(value);
+
+        if (this.value.isBlank()){
+            throw new IllegalArgumentException("El nombre del cajero no puede estar vacío");
+        }
+
+        if(this.value.length() < 4){
+            throw new IllegalArgumentException("Ingrese un nombre válido");
+        }
+    }
+
+    public String value(){
+        return value;
+    }
     
 }
