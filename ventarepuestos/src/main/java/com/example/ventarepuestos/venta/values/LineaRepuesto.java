4bd402fa1e5b21c57a7131efa82cf017370c2c3b
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 18:15:54 -0500

    PE. Objeto de valor LineaRepuesto terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/LineaRepuesto.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/LineaRepuesto.java
@@ -1,5 +1,27 @@
 package com.example.ventarepuestos.venta.values;
 
-public class LineaRepuesto {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class LineaRepuesto implements ValueObject<String>{
+
+    private final String value;
+
+    public LineaRepuesto(String value){
+        this.value = Objects.requireNonNull(value);
+
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El campo línea no puede estar vacío");
+        }
+
+        if(this.value.length() < 5){
+            throw new IllegalArgumentException("La línea del repuesto no puede ser menor de 5 caracteres");
+        }
+    }
+
+    public String value(){
+        return value;
+    }
     
 }
