b7ff32a503c03351a08fdbf8d9ea5238210d2124
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 17:44:49 -0500

    PE. Objeto de valor RepuestoId terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/RepuestoId.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/RepuestoId.java
@@ -3,5 +3,17 @@ package com.example.ventarepuestos.venta.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class RepuestoId extends Identity{
+
+    public RepuestoId(){
+
+    }
+
+    public RepuestoId(String id){
+        super(id);
+    }
+
+    public static RepuestoId of(String id){
+        return new RepuestoId(id);
+    }
     
 }
