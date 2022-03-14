ad5ccbeb1aca4efc0768a8f6b07e61b6fcb88a33
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-03-14 18:44:22 -0500

    PE. Objeto de valor NombreRepuesto terminado

--- ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/NombreRepuesto.java
+++ ventarepuestos/src/main/java/com/example/ventarepuestos/venta/values/NombreRepuesto.java
@@ -1,5 +1,27 @@
 package com.example.ventarepuestos.venta.values;
 
-public class NombreRepuesto {
+import java.util.Objects;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class NombreRepuesto implements ValueObject<String>{
+
+    private final String value;
+
+    public NombreRepuesto(String value){
+        this.value = Objects.requireNonNull(value);
+
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El nombre del repuesto no puede estar vacío");
+        }
+
+        if(this.value.length() < 5){
+            throw new IllegalArgumentException("Solo se admiten nombres de repuestos de al menos 5 caracteres");
+        }
+    }
+
+    public String value(){
+        return value;
+    }
     
 }
